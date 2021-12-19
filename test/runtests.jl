5bcd7a679d3d95a592c77fca11c8ea7e1d7fbedb
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2021-12-19 17:46:46 -0500

    test another code path

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ SlackThreads.CATCH_EXCEPTIONS[] = false
 try
     @testset "SlackThreads.jl" begin
         if VERSION >= v"1.7"
-            @testset "JET with args $args" for args in [("hi",), ("hi", "str" => "hello")]
+            @testset "JET with args $args" for args in [("hi",), ("hi", "str" => "hello"), ("hi", "str.txt" => "hello", "a" => "b")]
                 thread = SlackThread()
                 @test_call target_modules = (SlackThreads,) thread(args...)
                 @test_call target_modules = (SlackThreads,) mode = :sound broken = true thread(args...)
